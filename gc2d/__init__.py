6e9ba1369d0e9afe04283f0497a41a0c8a870246
Wouter Brink <wouterr.brink@hotmail.com>
2019-03-13 20:45:41 +0100

    Init main frame

--- gc2d/__init__.py
+++ gc2d/__init__.py
@@ -1,1 +1,28 @@
+import Tkinter
+import tkMessageBox
 
+top = Tkinter.Tk()
+graphCanvas = Tkinter.Canvas(top, width=640, height=480, bg='darkgrey')
+
+
+def render2D():
+    graphCanvas.configure(bg='grey')
+
+    # TODO: Insert code to generate the graph
+
+    # ...
+
+    print("Rendered 2D graph")
+
+def initialize_mainframe():
+    # Initialize mainframe
+
+    # Initialize widgets
+    graphCanvas.pack()
+
+    render_button = Tkinter.Button(top, text ="Generate 2D", command =render2D)
+    render_button.pack()
+    top.mainloop()
+
+
+initialize_mainframe()
