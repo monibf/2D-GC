b50112f5d39505b457f7b7f42b037e392680809b
Wouter Brink <wouterr.brink@hotmail.com>
2019-03-14 20:58:38 +0100

    Improve initialising main frame

--- gc2d/MainWindow.py
+++ gc2d/MainWindow.py
@@ -1,45 +1,50 @@
 import Tkinter
 
-class MainWindow:
 
+class MainWindow:
+    """" The main window class. Creates and con the global components of the UI """
     top = Tkinter.Tk()
 
     def __init__(self):
         self.init_mainframe()
 
     def render2D(self):
+        """" Function to render the graph and assign it to the frame """
+
         # TODO: Insert code to render graph
         # Should be done by calling a controller
         # ...
 
         print("Rendered 2D graph")
 
-    def init_top_frame(self):
+    def init_top_frame(self, frame_height, graph_width, sidebar_width):
         """" Initialize the top frame. This includes the graph and graph control frame """
-        graph_width = 640
-        graph_height = 480
 
-        sidebar_width = 100
-
-        top_frame = Tkinter.Frame(self.top, height=graph_height, width=graph_width + sidebar_width, bg='gray')
+        top_frame = Tkinter.Frame(self.top, height=frame_height, width=graph_width + sidebar_width, bg='gray')
         top_frame.pack(side='top')
 
-        left_frame = Tkinter.Frame(top_frame, height=graph_height, width=sidebar_width, bg='darkgray')
+        left_frame = Tkinter.Frame(top_frame, height=frame_height, width=sidebar_width, bg='darkgray')
         left_frame.pack(side='left')
 
-        graph_frame = Tkinter.Frame(top_frame, height=graph_height, width=graph_width, bg='lightgray')
+        graph_frame = Tkinter.Frame(top_frame, height=frame_height, width=graph_width, bg='lightgray')
         graph_frame.pack(side='right')
 
-    def init_bottom_frame(self):
+    def init_bottom_frame(self, frame_height, frame_width):
         """" Initialize the top frame. This includes other controls """
 
-        bottom_frame = Tkinter.Frame(self.top, height=80, width=640 + 100, bg='gray26')
+        bottom_frame = Tkinter.Frame(self.top, height=frame_height, width=frame_width, bg='gray26')
         bottom_frame.pack(side='bottom')
 
     def init_mainframe(self):
         """ Initialize the main frame. This includes every UI element """
 
-        self.init_top_frame()
-        self.init_bottom_frame()
+        top_height = 480
+        bottom_height = 80
+
+        graph_width = 640
+        sidebar_width = 100
+
+        self.init_top_frame(frame_height=top_height, graph_width=graph_width, sidebar_width=sidebar_width)
+        self.init_bottom_frame(frame_height=bottom_height, frame_width=graph_width + sidebar_width)
 
         self.top.mainloop()
