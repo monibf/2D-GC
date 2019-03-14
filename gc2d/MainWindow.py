4cbdc2eb4baf2611eb89f51e0ffe4df9e7b10634
Wouter Brink <wouterr.brink@hotmail.com>
2019-03-14 21:48:24 +0100

    Added necessary objects from matplotlib

--- gc2d/MainWindow.py
+++ gc2d/MainWindow.py
@@ -1,39 +1,16 @@
-import Tkinter
+import Tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 
 
 class MainWindow:
     """" The main window class. Creates and con the global components of the UI """
-    top = Tkinter.Tk()
+    top = tk.Tk()
 
     def __init__(self):
         self.init_mainframe()
-
-    def render2D(self):
-        """" Function to render the graph and assign it to the frame """
-
-        # TODO: Insert code to render graph
-        # Should be done by calling a controller
-        # ...
-
-        print("Rendered 2D graph")
-
-    def init_top_frame(self, frame_height, graph_width, sidebar_width):
-        """" Initialize the top frame. This includes the graph and graph control frame """
-
-        top_frame = Tkinter.Frame(self.top, height=frame_height, width=graph_width + sidebar_width, bg='gray')
-        top_frame.pack(side='top')
-
-        left_frame = Tkinter.Frame(top_frame, height=frame_height, width=sidebar_width, bg='darkgray')
-        left_frame.pack(side='left')
-
-        graph_frame = Tkinter.Frame(top_frame, height=frame_height, width=graph_width, bg='lightgray')
-        graph_frame.pack(side='right')
-
-    def init_bottom_frame(self, frame_height, frame_width):
-        """" Initialize the top frame. This includes other controls """
-
-        bottom_frame = Tkinter.Frame(self.top, height=frame_height, width=frame_width, bg='gray26')
-        bottom_frame.pack(side='bottom')
 
     def init_mainframe(self):
         """ Initialize the main frame. This includes every UI element """
@@ -44,7 +21,51 @@ class MainWindow:
         graph_width = 640
         sidebar_width = 100
 
+        self.top.title("2Dx2D GC")
+
         self.init_top_frame(frame_height=top_height, graph_width=graph_width, sidebar_width=sidebar_width)
         self.init_bottom_frame(frame_height=bottom_height, frame_width=graph_width + sidebar_width)
 
         self.top.mainloop()
+
+    def init_top_frame(self, frame_height, graph_width, sidebar_width):
+        """" Initialize the top frame. This includes the graph and graph control frame """
+
+        top_frame = tk.Frame(self.top, height=frame_height, width=graph_width + sidebar_width, bg='gray')
+        top_frame.pack(side='top')
+
+        left_frame = tk.Frame(top_frame, height=frame_height, width=sidebar_width, bg='darkgray')
+        left_frame.pack(side='left')
+
+        graph_frame = tk.Frame(top_frame, height=frame_height, width=graph_width, bg='lightgray')
+        graph_frame.pack(side='right')
+
+    def init_bottom_frame(self, frame_height, frame_width):
+        """" Initialize the top frame. This includes other controls """
+
+        bottom_frame = tk.Frame(self.top, height=frame_height, width=frame_width, bg='gray26')
+        bottom_frame.pack(side='bottom')
+
+    def render2D(self):
+        """" Function to render the graph and assign it to the frame """
+        """" As of right now, this is a test function to see if we can render the matplotlib on a Tkinter Frame """
+        arr = self.read_data("MF_AE3.txt")
+        grid = np.transpose(arr)
+        plt.imshow(grid, clim=(1e4, 1e6), origin="lower")
+        plt.colorbar()
+        plt.show()
+
+        # TODO: Insert code to render graph
+        # Should be done by calling a controller
+        # ...
+
+        print("Rendered 2D graph")
+
+    def read_data(self, filename):
+        data = []
+        with open(filename) as sourcefile:
+            for line in sourcefile:
+                row = [float(val) for val in line.split(", ") if val.strip()]
+                data.append(row)
+        arr = np.array(data, dtype=np.float64)
+        return arr
