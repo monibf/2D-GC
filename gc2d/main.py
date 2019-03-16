cd858e6d1b244f9f46abadef18155f750d27d42f
troido <troido@protonmail.com>
2019-03-16 18:29:40 +0100

    moved data to folder exampledata

--- gc2d/main.py
+++ gc2d/main.py
@@ -7,7 +7,7 @@ from gc2d.MainWindow import MainWindow
 def main():
     
     window = MainWindow()
-    datafile = os.path.join(os.path.dirname(__file__), "MF_AE3.txt")
+    datafile = os.path.join(os.path.dirname(__file__), "..", "exampledata", "MF_AE3.txt")
     window.load_chromatogram(datafile)
     
     window.top.mainloop()
