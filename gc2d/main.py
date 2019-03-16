a2b4acd775e1d6259099d24632aad75a949747a0
troido <troido@protonmail.com>
2019-03-16 09:58:54 +0100

    fixed spelling error and directory error when loading data file

--- gc2d/main.py
+++ gc2d/main.py
@@ -1,13 +1,17 @@
 
+import os.path
+
 from gc2d.MainWindow import MainWindow
 from gc2d.render.renderer2d import Renderer2d
 from gc2d.model.chromatogram import Chromatogram
+
 
 def main():
     
     window = MainWindow()
     render = Renderer2d(window.graph_frame)
-    chrom = Chromatograph.from_file("MF_AE3.txt")
+    datafile = os.path.join(os.path.dirname(__file__), "MF_AE3.txt")
+    chrom = Chromatogram.from_file(datafile)
     render.render2d(chrom.as_grid())
     
     window.top.mainloop()
