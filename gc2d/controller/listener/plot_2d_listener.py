f63ab8ac98a1660fd92c61723bc1e68941e4ac07
Kranex <oliverstrik@gmail.com>
2019-03-27 14:33:53 +0100

    worked out how to call the original methods. Took the liberty of documenting the code and removing the noisy functions.

--- gc2d/controller/listener/plot_2d_listener.py
+++ gc2d/controller/listener/plot_2d_listener.py
@@ -1,7 +1,13 @@
-from gc2d.controller.listener.plot_listener import PlotListener
+from gc2d.controller.listener.widget_listener import WidgetListener
 
 
-class Plot2DListener(PlotListener):
+class Plot2DListener(WidgetListener):
 
     def __init__(self, plot2d, model_wrapper):
-        super().__init__(plot2d, model_wrapper)
+        """
+        A stub listener for the plot_2d_widget
+        :param plot2d: the plot_2d_widget
+        :param model_wrapper: the model wrapper
+        """
+        super().__init__(plot2d)
+        self.model_wrapper = model_wrapper
--- gc2d/controller/listener/plot_3d_listener.py
+++ gc2d/controller/listener/plot_3d_listener.py
@@ -1,8 +1,13 @@
-from gc2d.controller.listener.plot_listener import PlotListener
+from gc2d.controller.listener.widget_listener import WidgetListener
 
 
-class Plot3DListener(PlotListener):
+class Plot3DListener(WidgetListener):
 
     def __init__(self, plot3d, model_wrapper):
-        super().__init__(plot3d, model_wrapper)
-
+        """
+        A stub listener for the plot_3d_widget
+        :param plot3d: the plot_3d_widget
+        :param model_wrapper: the model wrapper
+        """
+        super().__init__(plot3d)
+        self.model_wrapper = model_wrapper
