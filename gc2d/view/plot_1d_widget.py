3a0bb72d15995758355127776646d13178b89ca1
Kranex <oliverstrik@gmail.com>
2019-04-13 17:51:01 +0200

    added 1D widget

---
+++ gc2d/view/plot_1d_widget.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from pyqtgraph import PlotWidget
+
+from gc2d.controller.listener.plot_2d_listener import Plot2DListener
+
+
+class Plot1DWidget(PlotWidget):
+
+    def __init__(self, model_wrapper, parent=None):
+        """
+        The Plot2DWidget is responsible for rendering the 2D chromatogram data.
+        :param model_wrapper: the wrapper of the model.
+        :param parent: the parent of this Widget.
+        """
+        super().__init__(parent=parent)
+
+        self.listener = Plot2DListener(self, model_wrapper)
+        self.curve = self.plot(pen='y')
+        self.notify('model', model_wrapper.model)
+        model_wrapper.add_observer(self, self.notify)
+
+    def notify(self, name, value):
+        """
+        Updates the image rendered to match the model.
+        :return: None
+        """
+
+        if name == 'model':
+            if value is None:
+                self.curve.setData([])
+            else:
+                self.curve.setData(np.sum(a=value.get_2d_chromatogram_data(), axis=1))
