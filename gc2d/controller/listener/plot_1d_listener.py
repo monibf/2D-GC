978edb3fac5464a2c0ae8209dc18854a62da6b16
Kranex <oliverstrik@gmail.com>
2019-04-14 14:56:36 +0200

    templated the 1D Scroll override

--- gc2d/controller/listener/plot_1d_listener.py
+++ gc2d/controller/listener/plot_1d_listener.py
@@ -1,3 +1,5 @@
+from PyQt5.QtCore import Qt
+
 from gc2d.controller.listener.widget_listener import WidgetListener
 
 
@@ -11,3 +13,29 @@ class Plot1DListener(WidgetListener):
         """
         super().__init__(plot1d)
         self.model_wrapper = model_wrapper
+
+    def mouse_scroll_event(self, event):
+        mods = event.modifiers()
+
+        ctrl = mods & Qt.ControlModifier
+        shift = mods & Qt.ShiftModifier
+        alt = mods & Qt.AltModifier
+
+        if ctrl and shift and alt:
+            print("Ctrl + Shift + Alt")
+        elif ctrl and shift:
+            print("Ctrl + Shift")
+        elif ctrl and alt:
+            print("Ctrl + Alt")
+        elif shift and alt:
+            print("Shift + Alt")
+        elif ctrl:
+            print("Ctrl")
+        elif shift:
+            print("Shift")
+        elif alt:
+            print("Alt")
+        else:
+            print("none")
+
+        super().mouse_scroll_event(event)
