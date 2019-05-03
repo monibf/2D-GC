4ad764a1f3dc430d1b4894bb1f6f943a5a7a9094
Kranex <OliverStrik@Gmail.com>
2019-05-04 00:27:30 +0200

    changed install location to %appdatalocal%\programs. EXE nolonger requires signing

--- INSTALL_WINDOWS.bat
+++ INSTALL_WINDOWS.bat
@@ -1,34 +1,5 @@
 @echo off
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------    
-
 echo Checking if python3 is installed...
 py -3 --version >NUL
 if errorlevel 1 goto npython
@@ -45,12 +16,13 @@ echo Installing dependencies...
 pip3 install -r requirements.txt
 
 echo Installing GC-2D...
-pyinstaller --name=2D-GC --noconsole --distpath="%PROGRAMFILES%" gc2d\__main__.py
+pyinstaller --name=2D-GC --noconsole --distpath="%LOCALAPPDATA%\Programs" gc2d\__main__.py
 
-mkdir "%PROGRAMFILES%\2D-GC\gc2d"
-mkdir "%PROGRAMFILES%\2D-GC\exampledata"
-copy exampledata "%PROGRAMFILES%\2D-GC\exampledata\"
-copy UNINSTALL_WINDOWS.bat "%PROGRAMFILES%\2D-GC\UNINSTALL.bat"
+mkdir "%LOCALAPPDATA%\Programs\2D-GC\gc2d"
+mkdir "%LOCALAPPDATA%\Programs\2D-GC\exampledata"
+copy exampledata "%LOCALAPPDATA%\Programs\2D-GC\exampledata\"
+copy UNINSTALL_WINDOWS.bat "%LOCALAPPDATA%\Programs\2D-GC\UNINSTALL.bat"
+
 mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\2D-GC"
 cscript GENERATE_LINK_WINDOWS.vbs
 copy 2D-GC.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\2D-GC"
