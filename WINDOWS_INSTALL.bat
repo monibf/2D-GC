55ba765ad8718a3d607541ff59a05e0de73af3cb
Kranex <OliverStrik@Gmail.com>
2019-05-04 15:56:21 +0200

    Installer will now offer to download and install python3 if either python or pip are missing. Can now be installed on a 100% clean system with only the install script.

--- WINDOWS_INSTALL.bat
+++ WINDOWS_INSTALL.bat
@@ -1,25 +1,29 @@
 @echo off
-if "%INSTALL_LOCATION%"=="" set INSTALL_LOCATION=%LOCALAPPDATA%
+if not defined INSTALL_LOCATION set INSTALL_LOCATION=%LOCALAPPDATA%
+set PYTHON_URL=https://www.python.org/ftp/python/3.7.3/python-3.7.3.exe
 set NAME=2D-GC
 set VERSION=0.1
 set PUBLISHER=RUG
+
+::----------------
+:begining
 echo Checking if python3 is installed...
 py -3 --version >NUL
-if errorlevel 1 goto npython
+if errorlevel 1 goto ipython
 
-echo Checking if pip3 is installed...
-pip3 -v >NUL
-if errorlevel 1 goto npip
+echo Checking if pip is installed...
+py -3 -m pip -v >NUL
+if errorlevel 1 goto ipip
 
 echo Checking if pyinstaller is installed...
-pyinstaller -v >NUL
-if errorlevel 1 pip install git+https://github.com/pyinstaller/pyinstaller.git
+py -3 -m PyInstaller -v >NUL
+if errorlevel 1 py -3 -m pip install git+https://github.com/pyinstaller/pyinstaller.git
 
 echo Installing dependencies...
-pip3 install -r requirements.txt
+py -3 -m pip install -r requirements.txt
 
 echo Installing %NAME%...
-pyinstaller --noconfirm --name="%NAME%" --noconsole --distpath="%INSTALL_LOCATION%" gc2d\__main__.py
+py -3 -m PyInstaller --noconfirm --name="%NAME%" --noconsole --distpath="%INSTALL_LOCATION%" gc2d\__main__.py
 
 :: This is required until the program opens without the example data.
 mkdir "%INSTALL_LOCATION%\%NAME%\gc2d"
@@ -49,19 +53,41 @@ mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\%NAME%"
 copy /Y "%NAME%.lnk" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\%NAME%" >NUL
 copy /Y "%NAME%.lnk" "%USERPROFILE%\Desktop" >NUL
 
-echo Done!
+:: Offer to uninstall python if we installed it.
+if defined INSTALLED_PYTHON goto upython
+
+::----------------
+:complete
+echo Installation complete!
 pause
 exit
+::---------------
+:ipython
+set /p yn=Download and install python ^(y/N^)^? 
+if /I not "%yn%"=="y" goto npython
+:: Download python if it hasn't been already...
+echo downloading python...
+if not exist python-installer.exe cscript WINDOWS_DOWNLOAD_PYTHON.vbs "%PYTHON_URL%" >NUL
 
+:: Prompt user to install python3.
+echo installing python3...
+python-installer.exe PrependPath=1 SimpleInstall=1 SimpleInstallDescription="Install the complete python suite and add python to the PATH."
+set INSTALLED_PYTHON=1
+start "" "%0"
+exit
+::----------------
+:upython
+set /p yn=The installation is now complete and python3 is no longer required, would you like to uninstall it ^(y/N^)? 
+if /I "%yn%"=="y" echo Uninstalling python3... & python-installer.exe /uninstall
+goto :complete
+::----------------
 :npython
 echo Error^: please install python3.
 pause
 exit
-n
-
-:npip
-echo Error^: please install pip3.
-pause
-exit
-
-
+::----------------
+:ipip
+echo Your current install of python3 does not include pip. 
+echo You can either install pip yourself, or this script can download and reinstall python%PYTHON_VERSION% for you.
+:goto ipython
+::----------------
