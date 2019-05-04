884006f28fd3ac38e8814f4e09145fe0e2500edb
Kranex <oliverstrik@gmail.com>
2019-05-04 21:46:24 +0200

    Linux works now

--- LINUX_INSTALL.sh
+++ LINUX_INSTALL.sh
@@ -11,19 +11,41 @@ if [[ $OSTYPE != "linux"* ]]; then
     exit 1
 fi
 
-./CHECK_DEPENDS.sh
-
-if [[ $? != 0 ]]; then
-    exit 1
-elif [[ -d "python3" ]]; then
-    # maybe one day I'll get this to work.
-    pass
-else
+printf "Checking python3 is installed: "
+if [[ -x $(command -v python3) ]]; then
+    printf "YES\n"
     PYTHON=$(command -v python3)
+else
+    printf "NO\n"
+    printf "Please install the latest version of python3 see the README.md for help."
+    exit 1
 fi
 
+printf "Checking pip3 is installed: "
+if [[ -x $(command -v pip3 --disable-pip-version-check) ]]; then
+    printf "YES\n"
+    PIP=$(command -v pip3 --disable-pip-version-check)
+else
+    printf "NO\n"
+    printf "Please install the latest version of pip see the README.md for help."
+    exit 1
+fi
+
+printf "Checking PyInstaller is installed: "
+if [[ $("$PIP" --disable-pip-version-check list | grep PyInstaller) ]]; then
+    printf "YES\n"
+else
+    printf "NO\n"
+    printf "Installing PyInstaller...\n"
+    "$PIP" install --user PyInstaller
+fi
+
+printf "Installing Dependencies...\n"
+
+"$PIP" install --user -r requirements.txt
+
 echo "Installing $NAME..."
-"$PYTHON" -m PyInstaller --noconfirm --onefile --windowed \
+"$PYTHON" -m PyInstaller --noconfirm --windowed \
  --distpath="$INSTALL_LOCATION" \
  --name="$NAME" gc2d/__main__.py
 if [[ $? != 0 ]]; then
