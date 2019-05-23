56063225fe92833a6e1896ed997aa2f58d53490c
Wouter Brink <wouterr.brink@hotmail.com>
2019-05-23 11:48:30 +0200

    Save 2D and 3D graph on ctrl + s when focus is on widget

--- gc2d/controller/listener/plot_3d_listener.py
+++ gc2d/controller/listener/plot_3d_listener.py
@@ -1,4 +1,5 @@
 from gc2d.controller.listener.widget_listener import WidgetListener
+from PyQt5.QtCore import Qt
 
 
 class Plot3DListener(WidgetListener):
@@ -11,3 +12,14 @@ class Plot3DListener(WidgetListener):
         """
         super().__init__(plot3d)
         self.model_wrapper = model_wrapper
+
+    def key_press_event(self, event):
+        mods = event.modifiers()
+        ctrl = mods & Qt.ControlModifier
+
+        if event.key() == Qt.Key_S and ctrl:
+            self.widget.grabFrameBuffer().save('fileName.png')
+            print('saved')
+
+        super().key_press_event(event)
+
