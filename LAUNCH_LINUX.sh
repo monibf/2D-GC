2a8122208b605b4fe900edccd21b9af50ba3ff98
Kranex <oliverstrik@gmail.com>
2019-05-23 12:13:34 +0200

    fixed for shortcut on path

--- LAUNCH_LINUX.sh
+++ LAUNCH_LINUX.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$( cd -p "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $(pwd)
 python3 -m gc2d $@
