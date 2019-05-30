c77d41eee1aa27a3a9049011527f1a6e7fb144e8
Kranex <oliverstrik@gmail.com>
2019-05-30 23:14:12 +0200

    choose palette action is disabled when there is no model

--- gc2d/controller/action/open_choose_palette_action.py
+++ gc2d/controller/action/open_choose_palette_action.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import QAction
 
 from gc2d.controller.dialogs.palette_chooser import PaletteChooser
 
-
 class OpenChoosePaletteAction(QAction):
 
     def __init__(self, parent, model_wrapper):
@@ -15,6 +14,8 @@ class OpenChoosePaletteAction(QAction):
         self.model_wrapper = model_wrapper
         self.setShortcut('Ctrl+Shift+C')
         self.setStatusTip('Opens the Choose Palette Dialog')
+        self.setEnabled(self.model_wrapper.model is not None)
+        self.model_wrapper.add_observer(self, self.notify)
         self.triggered.connect(self.show_dialog)
 
     # noinspection PyArgumentList
@@ -26,3 +27,7 @@ class OpenChoosePaletteAction(QAction):
         i = -1
         while i == -1:
             i = PaletteChooser(self.parent(), self.model_wrapper)
+
+    def notify(self, name, value):
+        if name == 'model':
+            self.setEnabled(value is not None)
