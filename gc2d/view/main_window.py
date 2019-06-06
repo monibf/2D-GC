2ee714cd56fa8cc82bd2a19f7039152ce7110c5e
Kranex <oliverstrik@gmail.com>
2019-06-06 21:52:01 +0200

    main_window.py 99% refactored

--- gc2d/view/main_window.py
+++ gc2d/view/main_window.py
@@ -49,35 +49,19 @@ class Window(QMainWindow):
         self.resize(500, 500)
         self.setWindowTitle('GCxGC')
 
+        self.toolbar = self.addToolBar("toolbar")
+
         self.plot_1d = None
         self.plot_2d = None
         self.plot_3d = None
 
-        self.open_file_action = OpenFileAction(self, self.model_wrapper)
-
         self.save_action = SaveAction(self, self.model_wrapper)
         self.save_as_action = SaveAsAction(self, self.model_wrapper, self.save_action)
-        self.save_integrations_action = SaveIntegrationsAction(self, self.model_wrapper)
-        self.save_prefs_action = SavePrefsAction(self, self.model_wrapper)
-        self.import_data_action = ImportDataAction(self, self.model_wrapper)
-
-        self.export_action = ExportAction(self, self.model_wrapper)
-        self.export_plot_2d_action = ExportPlot2DAction(self, self.model_wrapper)
-        self.export_plot_3d_action = ExportPlot3DAction(self, self.model_wrapper)
-
-        self.exit_action = ExitAction(self)
-        self.draw_action = DrawAction(self, self.model_wrapper)
-        self.open_edit_axes_action = OpenEditAxesAction(self, self.model_wrapper)
-        self.open_palette_chooser_action = OpenChoosePaletteAction(self, self.model_wrapper)
-        self.open_convolution_picker_action = OpenConvolutionPickerAction(self, self.model_wrapper)
-        self.toggle_convolution_action = ToggleConvolutionAction(self, self.model_wrapper)
-
-        status_bar = self.statusBar()
 
         # create UI elements.
         self.create_menus()  # Create the menus in the menu bar.
         self.create_toolbar()
-        self.create_graph_views(status_bar)  # Create 2D and 3D dock tabs.
+        self.create_graph_views()  # Create 2D and 3D dock tabs.
 
         self.show()  # Show the window.
 
@@ -91,46 +75,44 @@ class Window(QMainWindow):
 
         # action objects need to be members because otherwise they get garbage collected
         file_menu = main_menu.addMenu('File')
-        file_menu.addAction(self.open_file_action)
+        file_menu.addAction(OpenFileAction(self, self.model_wrapper))
 
-        file_menu.addAction(self.import_data_action)
+        file_menu.addAction(ImportDataAction(self, self.model_wrapper))
         file_menu.addAction(self.save_action)
         file_menu.addAction(self.save_as_action)
-        file_menu.addAction(self.save_integrations_action)
-        file_menu.addAction(self.save_prefs_action)
+        file_menu.addAction(SaveIntegrationsAction(self, self.model_wrapper))
+        file_menu.addAction(SavePrefsAction(self, self.model_wrapper))
 
         file_menu.addSeparator()
-        file_menu.addAction(self.export_plot_2d_action)
-        file_menu.addAction(self.export_plot_3d_action)
+        file_menu.addAction(ExportPlot2DAction(self, self.model_wrapper))
+        file_menu.addAction(ExportPlot3DAction(self, self.model_wrapper))
 
-        file_menu.addAction(self.exit_action)
+        file_menu.addAction(ExitAction(self))
 
         edit_menu = main_menu.addMenu('Edit')
-        edit_menu.addAction(self.draw_action)
-        edit_menu.addAction(self.open_edit_axes_action)
+        edit_menu.addAction(DrawAction(self, self.model_wrapper))
+        edit_menu.addAction(OpenEditAxesAction(self, self.model_wrapper))
 
         view_menu = main_menu.addMenu('View')
-        view_menu.addAction(self.open_palette_chooser_action)
-        view_menu.addAction(self.toggle_convolution_action)
+        view_menu.addAction(OpenChoosePaletteAction(self, self.model_wrapper))
+        view_menu.addAction(ToggleConvolutionAction(self, self.model_wrapper))
 
         tools_menu = main_menu.addMenu('Tools')
-        tools_menu.addAction(self.open_convolution_picker_action)
+        tools_menu.addAction(OpenConvolutionPickerAction(self, self.model_wrapper))
         # TODO
 
         help_menu = main_menu.addMenu('Help')
         # TODO
     
     def create_toolbar(self):
-        
-        self.toolbar = self.addToolBar("toolbar")
-        self.toolbar.addAction(self.toggle_convolution_action)
-        self.toolbar.addAction(self.open_convolution_picker_action)
-        self.toolbar.addAction(self.open_palette_chooser_action)
-        self.toolbar.addAction(self.draw_action)
-        self.toolbar.addAction(self.export_action)
+        self.toolbar.addAction(self.ToggleConvolutionAction(self, self.model_wrapper))
+        self.toolbar.addAction(OpenConvolutionPickerAction(self, self.model_wrapper))
+        self.toolbar.addAction(OpenChoosePaletteAction(self, self.model_wrapper))
+        self.toolbar.addAction(DrawAction(self, self.model_wrapper))
+        self.toolbar.addAction(ExportAction(self, self.model_wrapper))
 
     # noinspection PyArgumentList
-    def create_graph_views(self, status_bar):
+    def create_graph_views(self):
         """
         Creates the window containing the graph views.
         :return: None. Later it should return a QWidget containing the views.
@@ -150,10 +132,10 @@ class Window(QMainWindow):
         self.plot_3d = Plot3DWidget(self.model_wrapper, dock_3d)
         dock_3d.addWidget(self.plot_3d)
 
-        self.plot_2d = Plot2DWidget(self.model_wrapper, status_bar, dock_2d)
+        self.plot_2d = Plot2DWidget(self.model_wrapper, self.statusBar(), dock_2d)
         dock_2d.addWidget(self.plot_2d)
 
-        self.plot_1d = Plot1DWidget(self.model_wrapper, status_bar, dock_1d)
+        self.plot_1d = Plot1DWidget(self.model_wrapper, self.statusBar(), dock_1d)
         dock_1d.addWidget(self.plot_1d)
 
         # TODO: move away from this function
