4589af8e5c4a31686948f50671f06cf2c3655966
Wouter Brink <wouterr.brink@hotmail.com>
2019-06-10 11:48:24 +0200

    Remove clipboard stuff

--- gc2d/controller/action/export_integration_list.py
+++ gc2d/controller/action/export_integration_list.py
@@ -8,17 +8,13 @@ class ExportIntegrationAction(QAction):
         :param parent: The parent widget
         :param model_wrapper: The Model Wrapper
         """
-        super().__init__('Copy Integration to Clipboard', parent)
+        super().__init__('Export Integrations to CSV File', parent)
         self.window = parent
         self.model_wrapper = model_wrapper
         self.status_bar = parent.status_bar
 
-        self.clipboard = QApplication.clipboard()
-        self.clipboard.clear(mode=self.clipboard.Clipboard)
-        self.clipboard.setText('Initialized')
-
         self.setShortcut('Ctrl+Y')
-        self.setStatusTip('Copy Integration to Clipboard')
+        self.setStatusTip('Export Integrations to CSV File')
         self.setEnabled(model_wrapper.model is not None)
         self.triggered.connect(self.export_integration_list)
 
@@ -26,32 +22,25 @@ class ExportIntegrationAction(QAction):
 
     def export_integration_list(self):
         """
-        Export the integration list to the clipboard
+        Export the integration list to a CSV file
         :return: bool if data was successfully exported
         """
 
         # Check if a plot is loaded
-        if self.window.plot_2d is not None:
+        if self.model_wrapper.model is not None:
 
-            # Copy the integration list
-            csv = self.create_csv_array()
-            self.clipboard.setText(csv)
-            self.status_bar.showMessage("Copied integration list to clipboard")
+            # Export the integration list
+            integration_array = self.model_wrapper.integrations.values()
+
+            csv = ""
+            for integration in integration_array:
+                csv += integration.label + ", " + \
+                       str(integration.mean) + ", " + str(integration.sum) + "\r\n"
+
             return True
 
         # Otherwise, no data was loaded.
-        print("No data loaded")
         return False
-
-    def create_csv_array(self):
-        integration_array = self.model_wrapper.integrations.values()
-
-        csv = ""
-        for integration in integration_array:
-            csv += str(integration.id + 1) + ", " + integration.label + ", " + \
-                   str(integration.mean) + ", " + str(integration.sum) + "\r\n"
-
-        return str(csv)
 
     def notified(self, name, model):
         if name == 'model':
