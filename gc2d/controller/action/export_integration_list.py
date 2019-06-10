d4ec6d8fdcc44babf389658531ef68fa3715de72
Wouter Brink <wouterr.brink@hotmail.com>
2019-06-10 12:17:59 +0200

    Export to CSV instead of copying to clipboard

--- gc2d/controller/action/export_integration_list.py
+++ gc2d/controller/action/export_integration_list.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QAction, QApplication
+from PyQt5.QtWidgets import QAction, QFileDialog
 
 
 class ExportIntegrationAction(QAction):
@@ -8,14 +8,14 @@ class ExportIntegrationAction(QAction):
         :param parent: The parent widget
         :param model_wrapper: The Model Wrapper
         """
-        super().__init__('Export Integrations to CSV File', parent)
+        super().__init__('Export to CSV', parent)
         self.window = parent
         self.model_wrapper = model_wrapper
 
         if shortcut is not None:
             self.setShortcut(shortcut)
 
-        self.setStatusTip('Export Integrations to CSV File')
+        self.setStatusTip('Export to CSV')
         self.setEnabled(model_wrapper.model is not None)
         self.triggered.connect(self.export_integration_list)
 
@@ -23,20 +23,24 @@ class ExportIntegrationAction(QAction):
 
     def export_integration_list(self):
         """
-        Export the integration list to a CSV file
+        Export the integration list to CSV
         :return: bool if data was successfully exported
         """
 
         # Check if a plot is loaded
         if self.model_wrapper.model is not None:
 
+            path = QFileDialog.getSaveFileName(self.window, 'Export to CSV')[0]
+            if path is '':
+                return
+
             # Export the integration list
             integration_array = self.model_wrapper.integrations.values()
 
-            csv = ""
-            for integration in integration_array:
-                csv += integration.label + ", " + \
-                       str(integration.mean) + ", " + str(integration.sum) + "\r\n"
+            with open(path, 'w') as file:
+                for integration in integration_array:
+                    line = '{0}, {1}, {2}\r\n'.format(integration.label, str(integration.mean), str(integration.sum))
+                    file.write(line)
 
             return True
 
